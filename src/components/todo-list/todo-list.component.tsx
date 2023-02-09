75dd87a76093df75b2386ffe89554f8fa636f7ca
marco.pereira <marco.pereira@First-Factory-Inc-Mac-H3MYCXHP29.local>
2023-02-09 15:51:12 -0600

    Clen up

--- src/components/todo-list/todo-list.component.tsx
+++ src/components/todo-list/todo-list.component.tsx
@@ -3,7 +3,7 @@ import {FlatList, FlatListProps} from 'react-native';
 import {TodoItem} from '../todo-item/todo-item.component';
 import styles from './todo-list.styles';
 
-interface Props extends FlatListProps<string> {}
+interface Props extends Partial<FlatListProps<string>> {}
 
 const TodoList: React.FC<Props> = ({data}) => {
   const renderItem = ({item}: {item: string}) => <TodoItem todo={item} />;
