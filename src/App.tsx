727f185e57c61004cf320ea3a818c5138335040f
marco.pereira <marco.pereira@First-Factory-Inc-Mac-H3MYCXHP29.local>
2023-02-09 11:48:05 -0600

    Add yaml file

---
+++ src/App.tsx
@@ -0,0 +1,46 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * Generated with the TypeScript template
+ * https://github.com/react-native-community/react-native-template-typescript
+ *
+ * @format
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  StatusBar,
+  Text,
+  View,
+  useColorScheme,
+} from 'react-native';
+
+const App = () => {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? '#ccc' : '#fff',
+    flex: 1,
+  };
+
+  return (
+    <SafeAreaView style={backgroundStyle}>
+      <StatusBar
+        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
+        backgroundColor={backgroundStyle.backgroundColor}
+      />
+      <View
+        style={{
+          flex: 1,
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}>
+        <Text style={{fontSize: 48}}>Todo App</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default App;
